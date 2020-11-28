119bdff60e2b88e9c6e588ea01e5354f77ab324c
Nnamani Ugochukwu F <royal.ugoh@gmail.com>
2020-11-28 16:37:47 +0100

    Add ejs template and Update app.js to receive post request

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hello');
+  let toDay = new Date();
+
+  const options = { weekday: 'long', day: 'numeric', month: 'long' };
+  const day = toDay.toLocaleString('en-US', options);
+
+  res.render('list', { kindOfDay: day });
+});
+
+app.post('/', (req, res) => {
+  const { task } = req.body;
+  console.log(task);
+  res.redirect('/');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
